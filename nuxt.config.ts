ae18f811074d6a4920ee8f3656521803658888cd
Andreas Ott <andreas.konrad.ott@googlemail.com>
2024-04-03 23:01:53 +0200

    updated env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL || 'http://localhost:8080',
+      baseURL: process.env.BASE_URL,
     }
   }
 })
