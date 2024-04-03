5484fc582e206be52eef0431f8b4942d90717b2c
Andreas Ott <andreas.konrad.ott@googlemail.com>
2024-04-03 14:32:19 +0200

    added development CI Pipeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,14 @@ export default defineNuxtConfig({
     '@/assets/style.css'
   ],
   modules: ['@sidebase/nuxt-auth'],
-    auth: {
-        provider: {
-            type: 'authjs'
-        }
+  auth: {
+    provider: {
+      type: 'authjs'
     }
+  },
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    }
+  }
 })
