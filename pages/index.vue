eae3fa2d4aa1adeac057323705b5fb4d133ac8d2
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-04-13 15:04:02 +0200

    test env var

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
        <div class="page">
+        <p>{{ useRuntimeConfig().public.eventService.baseURL }} + {{ useRuntimeConfig().public.ticketService.baseURL }}</p>
         <div class="login-container">
             <h1>Benevolo Login</h1>
             <div class="social-logins">
