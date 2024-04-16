4eff1f5304245fe132b26ce84fe5e326a4456fbd
Felix <fes2285@thi.de>
2024-04-16 09:46:53 +0200

    Authorization token  added to tickettype requests

--- requests/tickettype.ts
+++ requests/tickettype.ts
@@ -5,10 +5,19 @@ function getBaseURL() {
     return useRuntimeConfig().public.eventService.baseURL;
 }
 
+function getToken() {
+    const token = useCookie('token')
+    return token.value
+}
+
 
 export function getAllTicketTypes(id: string, onSuccess: (TicketType: TicketType[]) => void, onError: () => void) {
     let baseURL = getBaseURL();
-    axios.get<TicketType[]>(baseURL + "/ticket-types?eventId=" + id)
+    axios.get<TicketType[]>(baseURL + "/ticket-types?eventId=" + id, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
     .then((response) => {
         onSuccess(response.data);
     })
@@ -19,7 +28,11 @@ export function getAllTicketTypes(id: string, onSuccess: (TicketType: TicketType
 
 export function saveTicketType(TicketType: TicketType, onSuccess: () => void, onError: () => void) {
     let baseURL = getBaseURL();
-    axios.post(baseURL + "/ticket-types", TicketType)
+    axios.post(baseURL + "/ticket-types", TicketType, {
+        headers: {
+            Authorization: `Bearer ${getToken()}`
+        }
+    })
     .then(() => {
         onSuccess();
     })
