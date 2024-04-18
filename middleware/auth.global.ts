95f550e8ca5bd6dc7142c18bfa9e2027922db5a2
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-04-18 14:54:13 +0200

    login based rendering in header + auth middleware

---
+++ middleware/auth.global.ts
@@ -0,0 +1,9 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+    const cookie = useCookie("token");
+    if(to.fullPath !== '/' && cookie.value == null) {
+        return navigateTo('/');
+    }
+    if(to.fullPath === '/' && cookie.value != null) {
+        return navigateTo('/veranstaltungen');
+    }
+});
