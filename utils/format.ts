9daaf3c6029e99454dae535c8421c88c0fd89fc5
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-06-24 19:24:28 +0200

    number formatting

--- utils/format.ts
+++ utils/format.ts
@@ -1,6 +1,9 @@
 export function formatPrice(price: number | undefined): string | undefined {
     if(price) {
-        return (price/100).toFixed(2) + "€";
+        return Number((price/100)).toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }) + " €";
     }
     return undefined;
 }
